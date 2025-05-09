2f973c3c77f69d9eb7e839d2b30fb38bd8d0e764
Justin Hunter <justin@pinata.cloud>
2025-05-09 16:53:48 -0500

    Updated retrieval to be for private only and return a download link

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,86 @@
-import { Hono } from 'hono'
-import { paymentMiddleware } from "x402-hono";
-import pin from "./routes/pin"
+import { Context, Hono, Next } from "hono";
+import pin from "./routes/pin";
 import retrieve from "./routes/retrieve";
-import { cors } from 'hono/cors';
+import { cors } from "hono/cors";
+import { paymentMiddleware } from "x402-hono";
 
 type Bindings = {
   PINATA_JWT: string;
-}
+};
 
-const app = new Hono<{ Bindings: Bindings }>()
+const PRICE_PER_GB = 0.1;
+const MONTHS = 12;
+
+const app = new Hono<{ Bindings: Bindings }>();
 app.use(cors());
-app.use(paymentMiddleware(
-  "0xaD73eafCAc4F4c6755DFc61770875fb8B6bC8A25", // your receiving wallet address
-  {  // Route configurations for protected endpoints
-    "/pin/public": {
-      price: "$0.10",
-      network: "base-sepolia",
-      config: {
-        description: "Pay2Pin",
-      }
-    },
-    "/pin/private": {
-      price: "$0.10",
-      network: "base-sepolia",
-      config: {
-        description: "Pay2Pin",
-      }
-    }, 
-    "/retrieve/private/*": {
-      price: "$0.0001",
-      network: "base-sepolia",
-      config: {
-        description: "Pay2Read",
-      }
-    }, 
-    "/retrieve/public/*": {
-      price: "$0.0001",
-      network: "base-sepolia",
-      config: {
-        description: "Pay2Read",
-      }
-    }, 
-    "/retrieve/test": {
-      price: "$0.001", 
-      network: "base-sepolia", 
-      config: {
-        description: "Pay2Read"
+
+const createDynamicPaymentMiddleware = (
+  receivingWallet: any,
+  baseConfig: any,
+  facilitatorConfig: any
+) => {
+  return async (c: Context, next: Next) => {
+    if (c.req.method === "POST") {
+      const { fileSize } = await c.req.json();
+
+      const fileSizeInGB = fileSize / (1024 * 1024 * 1024);
+      const price = fileSizeInGB * PRICE_PER_GB * MONTHS;
+      console.log(price);
+      const priceToUse = price >= 0.0001 ? price : 0.0001;
+      console.log({priceToUse})
+      baseConfig = {
+        "/pin/public": {
+          price: `$${priceToUse.toFixed(4)}`,
+          network: "base-sepolia",
+          config: {
+            description: "Pay2Pin",
+          },
+        },
+        "/pin/private": {
+          price: `$${priceToUse.toFixed(4)}`,
+          network: "base-sepolia",
+          config: {
+            description: "Pay2Pin",
+          },
+        },
+      };
+    } else {
+      baseConfig = {
+        "/retrieve/private/*": {
+          price: `$0.0001`,
+          network: "base-sepolia",
+          config: {
+            description: "Pay2Read",
+          },
+        },        
       }
     }
-  },
-  {
-    url: "https://x402.org/facilitator", // Facilitator URL for Base Sepolia testnet.
-  }
-));
+
+    console.log(baseConfig);
+
+    const dynamicPaymentMiddleware = paymentMiddleware(
+      receivingWallet,
+      baseConfig,
+      facilitatorConfig
+    );
+
+    return dynamicPaymentMiddleware(c, next);
+  };
+};
+
+app.use(
+  createDynamicPaymentMiddleware(
+    "0xaD73eafCAc4F4c6755DFc61770875fb8B6bC8A25",
+    {},
+    { url: "https://x402.org/facilitator" }
+  )
+);
 
 app.get("/", (c) => {
-  return c.text("Hello")
-})
-app.route("/pin", pin)
-app.route("/retrieve", retrieve)
+  return c.text("Hello");
+});
 
-export default app
+app.route("/pin", pin);
+app.route("/retrieve", retrieve);
+
+export default app;
--- src/routes/retrieve.ts
+++ src/routes/retrieve.ts
@@ -1,11 +1,7 @@
 import { Hono } from "hono";
-import { paymentMiddleware } from "x402-hono";
-import { PaymentPayload } from "x402/types";
 import { PinataSDK } from "pinata";
 import { cors } from "hono/cors";
 import { Bindings } from "../utils/types";
-
-type Network = "public" | "private";
 
 const app = new Hono<{ Bindings: Bindings }>();
 app.use(cors());
@@ -14,23 +10,12 @@ app.get("/test", async (c) => {
   return c.text("Working!");
 });
 
-app.get("/:network/:cid", async (c) => {
+app.get("/private/:cid", async (c) => {
   try {
-    console.log("Request made...");
-    const network = c.req.param("network") as Network;
     const cid = c.req.param("cid");
-    console.log({ network, cid });
+    console.log({ cid });
     if (!cid) {
       return c.json({ message: "CID is required" }, 400);
-    }
-
-    if (network !== "public" && network !== "private") {
-      return c.json({ message: "Network must be 'public' or 'private'" }, 400);
-    }
-
-    if (!c.env.PINATA_JWT) {
-      console.error("Missing PINATA_JWT environment variable");
-      return c.json({ message: "Server configuration error" }, 500);
     }
 
     const pinata = new PinataSDK({
@@ -39,29 +24,12 @@ app.get("/:network/:cid", async (c) => {
       pinataGatewayKey: c.env.PINATA_GATEWAY_KEY,
     });
 
-    let response: any;
-    let url = "";
-    if (network === "public") {
-      url = `https://${c.env.PINATA_GATEWAY_URL}/ipfs/${cid}?pinataGatewayToken=${c.env.PINATA_GATEWAY_KEY}`;
-      response = await fetch(url);
-    } else {
-      url = await pinata.gateways.private.createAccessLink({
-        cid: cid,
-        expires: 3000,
-      });
-      response = await fetch(url);
-    }
+    const url = await pinata.gateways.private.createAccessLink({
+      cid: cid,
+      expires: 3000,
+    });
 
-    if (!response.ok) {
-      return c.text(`Failed to fetch from ${url}`, response.status);
-    }
-
-    const contentType =
-      response.headers.get("content-type") || "application/octet-stream";
-
-    c.header("content-type", contentType);
-
-    return c.newResponse(response.body, 200);
+    return c.json({ url: url });
   } catch (error) {
     console.error("Error retrieving from IPFS:", error);
 
